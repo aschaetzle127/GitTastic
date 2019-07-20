3246faf9079a6f4425bf0fb6a84acc6b587118ed
Lexi127 <aperalta127@gmail.com>
2019-07-20 11:19:59 -0700

    add button working

--- js/logic.js
+++ js/logic.js
@@ -1,78 +1,70 @@
 $(document).ready(function() {
-  var startGifs = ["Friends", "Party Down", "New Girl"];
+  var startGifs = ["Friends", "New Girl", "30 Rock"];
 
-  function buttonsAppear(buttonArray, classAdd, buttonDiv) {
-    $(buttonDiv).empty();
+  function makeButtons() {
+    $("#buttonsView").empty();
 
-    for (var i = 0; i < buttonArray.length; i++) {
+    for (var i = 0; i < startGifs.length; i++) {
       var a = $("<button>");
-
-      a.addClass(classAdd);
-      a.attr("data-type", buttonArray[i]);
-      a.text(buttonArray[i]);
-      $(buttonDiv).append(a);
+      a.addClass("showGifs");
+      a.attr("data-name", startGifs[i]);
+      a.text(startGifs[i]);
+      $("#buttonsView").append(a);
     }
   }
 
-  $(document).on("click", ".gif-button", function() {
-    $("#gifs").empty();
-    $(".git-button").removeClass("active");
-    $(this).addClass("active");
-
-    var type = $(this).attr("data-type");
-    var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" +
-      type +
-      "&api_key=3foLy7DcuYZeUjqgPerhOsrwTNnRcY6r";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      var results = response.data;
-
-      for (var i = 0; i < results.length; i++) {
-        var gifDiv = $('<div class="gif-item">');
-        var rating = results[i].rating;
-        var p = $("<p>").text("Rating: " + rating);
-        var movingGif = results[i].images.fixed_height.url;
-        var stillImage = results[i].images.fixed_height_still.url;
-        var gifImage = $("<img>");
-
-        gifImage.attr("src", stillImage);
-        gifImage.attr("data-still", stillImage);
-        gifImage.attr("data-move", movingGif);
-        gifImage.attr("data-state", "still");
-        gifImage.addClass("gif-image");
-
-        gifDiv.append(p);
-        gifDiv.append(gifImage);
-
-        $("#gifs").append(gifDiv);
-      }
-    });
+  // add the show gifs
+  $("#addGif").on("click", function() {
+    var newGifs = $("#gif-input")
+      .val()
+      .trim();
+    startGifs.push(newGifs);
+    makeButtons();
+    return false;
   });
 
-  $(document).on("click", ".gif-Image", function() {
-    var state = $(this).attr("data-state");
+  // function for displaying the giphy gifs
+  function displayGifs() {
+    var gif = $(this).attr("data-name");
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      gif +
+      "&api_key=3foLy7DcuYZeUjqgPerhOsrwTNnRcY6r";
 
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-still"));
+    // creates ajax call
+    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
+      console.log(response.data);
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class=gifs>");
+        var showGif = $("<img>");
+        showGif.attr("src", results[i].images.fixed_height_still.url);
+        showGif.attr("title", "Rating: " + results[i].rating);
+        showGif.attr("data-still", results[i].images.fixed_height_still.url);
+        showGif.attr("data-state", "still");
+        showGif.addClass("gif");
+        showGif.attr("data-animate", results[i].images.fixed_height.url);
+        gifDiv.append(showGif);
+
+        $("#gifsView").prepend(gifDiv);
+      }
+    });
+  }
+
+  // function for the animating
+  $(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    if (state == "still") {
+      $(this).attr("src", $(this).data("animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).data("still"));
       $(this).attr("data-state", "still");
     }
   });
 
-  $("#add-gif").on("click", function(event) {
-    event.preventDefault();
-    var newGif = $("input")
-      .eq(0)
-      .val();
+  // function for the displaying
+  $(document).on("click", ".showGifs", displayGifs);
 
-    if (newGif > 2) {
-      startGifs.push(newGif);
-    }
-
-    buttonsAppear(startGifs, "gif-button", "#gif-buttons");
-  });
-  buttonsAppear(startGifs, "gif-button", "#gif-buttons");
+  makeButtons();
 });
